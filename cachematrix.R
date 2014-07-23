daf348a3b2914066608dc7b3836ad9807654ca45
huch0474 <huaiying.chin@colorado.edu>
2014-07-23 10:11:18 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Basically I only replaced the "mean" function in the example 
+## to "solve" function to create the matrix I want
 
-## Write a short comment describing this function
+
+## Using "solve" function to set up the matrix I want
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+	}
+	get<-function() x
+	setmatrix<-function(solve) m<<-solve
+	getmatrix<-function() m
+	list(set=set,get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## get the inverse of the matrix we got
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m<-x$getmatrix()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+    	}
+    	data<-x$get()
+    	m<-solve(data, ...)
+    	x$setmatrix(m)
+   	m
 }
